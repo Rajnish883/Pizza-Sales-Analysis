93d15e43b2c321ba34826ae1e2e5fd5acea8c509
Rajnish883 <104516301+Rajnish883@users.noreply.github.com>
2025-07-10 20:10:59 +0530

    Delete Pizzahut directory

--- Pizzahut/SQL Files 6.sql
+++
@@ -1,9 +0,0 @@
---  Average Pizzas Per Order:
-
-
-SELECT 
-    ROUND(SUM(quantity) / COUNT(DISTINCT order_id),
-            2) AS Average_pizza_per_order
-FROM
-    order_details
-    
--- Pizzahut/SQL Files 8.sql
+++
@@ -1,7 +0,0 @@
--- Monthly Trend for Total Orders :
-
-select monthname(order_date) as order_month, count(distinct order_id) as total_orders
-from orders group by monthname(order_date)
-
-
- 
